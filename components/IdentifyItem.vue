c3179d0f7991369c7baa741d4447fd8d9d763a31
Cameron <cclarkjh@gmail.com>
2020-09-25 10:37:31 +0100

    fix(identify items): make identifying items less bad

--- components/IdentifyItem.vue
+++ components/IdentifyItem.vue
@@ -1,38 +1,101 @@
 <template>
-  <div class="identifyItemContainer">
-    <h2 class="mb-3">{{ $t('identify') }}</h2>
-    <v-text-field
-      v-model="itemName"
-      outlined
-      :label="$t('name')"
-      type="text"
-      @keyup.enter="lookupData"
-    />
-    <v-btn
-      color="primary"
-      :disabled="loading"
-      :loading="loading"
-      @click="lookupData"
-      >{{ $t('search') }}</v-btn
-    >
-    <v-checkbox v-model="replaceImages" label="Replace Existing Images" />
-    <div>
-      <v-card
-        v-for="result in searchResults"
-        :key="result.ProviderIds.Imdb"
-        class="mb-5"
-        @click="setItem(result)"
+  <v-stepper v-model="identifyStage">
+    <v-stepper-header>
+      <v-stepper-step
+        :complete="identifyStage > 1"
+        :editable="identifyStage > 1"
+        step="1"
+        >{{ $t('search') }}</v-stepper-step
       >
-        <v-img v-if="result.ImageUrl" :src="getSearchImage(result.ImageUrl)" />
-        <v-card-title>{{ result.Name }}</v-card-title>
-        <v-card-subtitle>{{ result.SearchProviderName }}</v-card-subtitle>
-      </v-card>
-      <v-card v-if="!searchResult">
-        <v-card-title>{{ $t('noItemsFound') }}</v-card-title>
-        <v-card-sub-title>{{ $t('widenQuery') }}</v-card-sub-title>
-      </v-card>
-    </div>
-  </div>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step
+        :complete="identifyStage > 2"
+        :editable="identifyStage > 2"
+        step="2"
+        >{{ $t('select') }}</v-stepper-step
+      >
+
+      <v-divider></v-divider>
+
+      <v-stepper-step step="3">{{ $t('confirm') }}</v-stepper-step>
+    </v-stepper-header>
+
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <v-text-field
+          v-model="itemName"
+          class="mt-3"
+          outlined
+          :label="$t('name')"
+          type="text"
+          @keyup.enter="lookupData"
+        />
+        <v-text-field
+          v-model="itemYear"
+          class="mt-3"
+          outlined
+          :label="$t('year')"
+          type="number"
+          @keyup.enter="lookupData"
+        />
+        <v-btn
+          color="primary"
+          :disabled="loading"
+          :loading="loading"
+          @click="lookupData"
+          >{{ $t('search') }}</v-btn
+        >
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
+        <v-card
+          v-for="result in searchResults"
+          :key="result.ProviderIds.Imdb"
+          class="mb-5"
+          @click="selectItem(result)"
+        >
+          <v-img
+            v-if="result.ImageUrl"
+            :src="getSearchImage(result.ImageUrl)"
+          />
+          <v-card-title>{{ result.Name }}</v-card-title>
+          <v-card-subtitle>{{ result.SearchProviderName }}</v-card-subtitle>
+        </v-card>
+        <v-card v-if="!searchResults.length">
+          <v-card-title>{{ $t('noItemsFound') }}</v-card-title>
+        </v-card>
+
+        <v-btn text @click="exit">{{ $t('cancel') }}</v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="3">
+        <v-card class="mb-5">
+          <v-img
+            v-if="selectedItem.ImageUrl"
+            :src="getSearchImage(selectedItem.ImageUrl)"
+          />
+          <v-card-title>{{ selectedItem.Name }}</v-card-title>
+          <v-card-subtitle>{{
+            selectedItem.SearchProviderName
+          }}</v-card-subtitle>
+        </v-card>
+
+        <v-checkbox v-model="replaceImages" label="Replace Existing Images" />
+        <v-btn
+          :disabled="loading"
+          :loading="loading"
+          color="primary"
+          @click="setItem(selectedItem)"
+        >
+          {{ $t('confirm') }}
+        </v-btn>
+
+        <v-btn text @click="exit">{{ $t('cancel') }}</v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
 </template>
 
 <script lang="ts">
@@ -50,23 +113,26 @@ export default Vue.extend({
   data() {
     return {
       itemName: '',
+      itemYear: undefined,
       searchResults: [] as RemoteSearchResult[],
       replaceImages: true,
-      loading: false
+      loading: false,
+      selectedItem: {} as RemoteSearchResult,
+      identifyStage: 1
     };
   },
   methods: {
     async lookupData() {
       this.loading = true;
+
       // TODO Add support for searching by IMDB/TVDB/Zap2It
-      // TODO Add support for filtering by adding production year
 
       switch (this.$props.item.Type) {
         case 'Series': {
           const response = await this.$itemLookupApi.getSeriesRemoteSearchResults(
             {
               seriesInfoRemoteSearchQuery: {
-                SearchInfo: { Name: this.itemName },
+                SearchInfo: { Name: this.itemName, Year: this.itemYear },
                 ItemId: this.$props.itemId
               }
             }
@@ -78,7 +144,7 @@ export default Vue.extend({
           const response = await this.$itemLookupApi.getMovieRemoteSearchResults(
             {
               movieInfoRemoteSearchQuery: {
-                SearchInfo: { Name: this.itemName },
+                SearchInfo: { Name: this.itemName, Year: this.itemYear },
                 ItemId: this.$props.itemId
               }
             }
@@ -88,18 +154,25 @@ export default Vue.extend({
         }
       }
 
+      this.identifyStage = 2;
       this.loading = false;
+    },
+    selectItem(item: RemoteSearchResult) {
+      this.selectedItem = item;
+      this.identifyStage = 3;
     },
     async setItem(info: RemoteSearchResult) {
       try {
+        this.loading = true;
         const response = await this.$itemLookupApi.applySearchCriteria({
           remoteSearchResult: info,
-          itemId: this.$props.itemId,
-          id: this.$props.itemId,
+          itemId: this.$props.item.Id,
+          id: this.$props.item.Id,
           replaceAllImages: this.replaceImages
         });
 
         if (response.status === 204) {
+          this.loading = false;
           this.$snackbar(this.$t('itemIdentified') as string, 'success');
           this.$emit('identified');
         }
@@ -109,6 +182,9 @@ export default Vue.extend({
     },
     getSearchImage(imageUrl: string) {
       return `${this.$axios.defaults.baseURL}/Items/RemoteSearch/Image?imageUrl=${imageUrl}&ProviderName=The%20Open%20Movie%20Database`;
+    },
+    exit() {
+      this.$emit('identified');
     }
   }
 });
